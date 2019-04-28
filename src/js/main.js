a437a8566f615aa8158402c5a2232953e04963f5
Paolo Stefan <paolostefan@gmail.com>
2019-04-28 09:56:06 +0200

    Shortcodes: wikipedia, twitter_hash, external_link

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,4 @@ import styles from './../css/main.css';
 
 import fontawesome from '@fortawesome/fontawesome'
 import brands from '@fortawesome/fontawesome-free-brands'
+import solid from '@fortawesome/fontawesome-free-solid'
